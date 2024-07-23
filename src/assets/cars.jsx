5f3e920b067a35b39cb889f2e2f432954b4008d5
KiwiScreams <screamskiwi3@gmail.com>
2024-07-24 01:44:13 +0400

    cars info

--- src/assets/cars.jsx
+++ src/assets/cars.jsx
@@ -1,112 +1,124 @@
-import image_1 from "../assets/images/car.png"
+import car1 from "../assets/images/cars/car-1.png"
+import car2 from "../assets/images/cars/car-2.png"
+import car3 from "../assets/images/cars/car-3.png"
+import car4 from "../assets/images/cars/car-4.png"
+import car5 from "../assets/images/cars/car-5.png"
+import car6 from "../assets/images/cars/car-6.png"
+import car7 from "../assets/images/cars/car-7.png"
+import car8 from "../assets/images/cars/car-8.png"
+import car9 from "../assets/images/cars/car-9.png"
+import car10 from "../assets/images/cars/car-10.png"
+import car11 from "../assets/images/cars/car-11.png"
+import car12 from "../assets/images/cars/car-12.png"
+
 export const cars =
     [
         {
             id: 1,
-            name: "car 1",
-            price: 100,
+            name: "Toyota Crown 2023",
+            price: 39900,
             monthPrice: 650,
-            image: image_1,
+            image: car1,
             description: "jgkfldjkjkjgkfldjkjkl",
             car: "car-1"
         },
         {
             id: 2,
-            name: "car 2",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Prius Prime 2023",
+            price: 39900,
+            monthPrice: 450,
+            image: car2,
             description: "jgkfldjkjkjgkfldjkjkl",
             car: "car-2"
         },
         {
             id: 3,
-            name: "car 3",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Toyota Prius 2023",
+            price: 39900,
+            monthPrice: 400,
+            image: car3,
             description: "jgkfldjkjkjgkfldjkjkl",
             car: "car-3"
         },
         {
             id: 4,
-            name: "car 1",
-            price: 100,
-            monthPrice: 650,
-            image: image_1,
+            name: "Hyundai Kona 2023",
+            price: 39900,
+            monthPrice: 510,
+            image: car4,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-1"
+            car: "car-4"
         },
         {
             id: 5,
-            name: "car 2",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Ford Explorer 2023",
+            price: 39900,
+            monthPrice: 1013,
+            image: car5,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-2"
+            car: "car-5"
         },
         {
             id: 6,
-            name: "car 3",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Santa  Fe 2023",
+            price: 39900,
+            monthPrice: 739,
+            image: car6,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-3"
+            car: "car-6"
         },
         {
             id: 7,
-            name: "car 1",
-            price: 100,
-            monthPrice: 650,
-            image: image_1,
+            name: "KIA Niro 2023",
+            price: 33600,
+            monthPrice: 554,
+            image: car7,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-1"
+            car: "car-7"
         },
         {
             id: 8,
-            name: "car 2",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Hyundai Tucson",
+            price: 31885,
+            monthPrice: 528,
+            image: car8,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-2"
+            car: "car-8"
         },
         {
             id: 9,
-            name: "car 3",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Honda CR-V 2023",
+            price: 35650,
+            monthPrice: 591,
+            image: car9,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-3"
+            car: "car-9"
         },
         {
             id: 10,
-            name: "car 1",
-            price: 100,
-            monthPrice: 650,
-            image: image_1,
+            name: "Hyundai Tucson",
+            price: 31885,
+            monthPrice: 528,
+            image: car10,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-1"
+            car: "car-10"
         },
         {
             id: 11,
-            name: "car 2",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Chevrolet Equinox",
+            price: 28430,
+            monthPrice: 471,
+            image: car11,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-2"
+            car: "car-11"
         },
         {
             id: 12,
-            name: "car 3",
-            price: 200,
-            monthPrice: 650,
-            image: image_1,
+            name: "Jeep Wrangler 2023",
+            price: 60000,
+            monthPrice: 997,
+            image: car12,
             description: "jgkfldjkjkjgkfldjkjkl",
-            car: "car-3"
+            car: "car-12"
         },
     ]
