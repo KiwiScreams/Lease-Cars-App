45e685daca2734956e1b582f02ecc73160322829
KiwiScreams <screamskiwi3@gmail.com>
2024-07-24 13:45:05 +0400

    contact form validation

--- src/pages/contact-page/Contact.jsx
+++ src/pages/contact-page/Contact.jsx
@@ -1,18 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import wheel from "../../assets/images/wheel.png"
 import './Contact.css'
 import Header from '../../components/header/Header'
 
 function Contact() {
   const [isSubmitted, setIsSubmitted] = useState(false);
-  const handleSubmit = () => {
-    setIsSubmitted(true);
-    setTimeout(() => {
-      setIsSubmitted(false);
-    }, 3000);
-  }
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errors, setErrors] = useState({});
+  const [successMessage, setSuccessMessage] = useState('');
   const name_regex = /^[a-zA-Z][a-zA-Z0-_]{3,23}$/;
   const email_regex = /^[a-zA-Z0-9]+@(?:[a-zA-Z0-9]+\.)+[A-Za-z]+$/;
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const errors = {};
+    if (!name_regex.test(name)) {
+      errors.name = 'Invalid username';
+    }
+    if (!email_regex.test(email)) {
+      errors.email = 'Invalid E-mail address';
+    }
+    if (message.trim() === '') {
+      errors.message = 'Message is required';
+    }
+    setErrors(errors);
+    if (Object.keys(errors).length === 0) {
+      setIsSubmitted(true);
+      setSuccessMessage('Message sent successfully!')
+      setTimeout(() => {
+        setIsSubmitted(false);
+        setSuccessMessage('');
+        setName('');
+        setEmail('');
+        setMessage('');
+      }, 3000);
+    }
+  }
   return (
     <>
       <section className='contact-container'>
@@ -20,18 +44,31 @@ function Contact() {
         <h2>Feel free to contact us with questions, potencial partnerships or media inquiries</h2>
         <div className='form'>
           <div className="inputs">
-            <input type="text" name="name" id="name" placeholder='Name' />
-            <input type="text" name="email" id="email" placeholder='E-mail' />
+            <div className="input">
+              <input type="text" name="name" id="name" placeholder='Name' value={name} onChange={(e) => setName(e.target.value)} />
+              {errors.name && <div className="error">{errors.name}</div>}
+            </div>
+            <div className="input">
+              <input type="text" name="email" id="email" placeholder='E-mail' value={email} onChange={(e) => setEmail(e.target.value)} />
+              {errors.email && <div className="error">{errors.email}</div>}
+            </div>
           </div>
-          <textarea name="" id="" placeholder='Your text'></textarea>
-          <p>Leave us a quick message <i class="fa-regular fa-message"></i></p>
+          <div className="input input-full">
+            <textarea name="" id="" placeholder='Your text' value={message} onChange={(e) => setMessage(e.target.value)}></textarea>
+            {errors.message && <div className="error">{errors.message}</div>}
+          </div>
+          {isSubmitted ? (
+            <p style={{ color: 'green' }}>{successMessage}</p>
+          ) : (
+            <p>Leave us a quick message <i className="fa-regular fa-message"></i></p>
+          )}
           <button className='btn' onClick={handleSubmit}>SUBMIT</button>
         </div>
       </section>
       <div className="social-container">
-        <a href="#"><i class="fa-brands fa-square-facebook"></i></a>
-        <a href="#"><i class="fa-brands fa-instagram"></i></a>
-        <a href="#"><i class="fa-brands fa-twitter"></i></a>
+        <a href="#"><i className="fa-brands fa-square-facebook"></i></a>
+        <a href="#"><i className="fa-brands fa-instagram"></i></a>
+        <a href="#"><i className="fa-brands fa-twitter"></i></a>
       </div>
       <img src={wheel} alt="" className={`wheel ${isSubmitted ? 'wheel-animation' : ''}`} />
 
