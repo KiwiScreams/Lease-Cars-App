b98a7b8c84cb8d3ebb2a3062cd3b39ee013078a8
KiwiScreams <screamskiwi3@gmail.com>
2024-07-24 12:15:07 +0400

    wheel animation

--- src/pages/contact-page/Contact.jsx
+++ src/pages/contact-page/Contact.jsx
@@ -4,21 +4,26 @@ import './Contact.css'
 import Header from '../../components/header/Header'
 
 function Contact() {
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const handleSubmit = () => 
+  {
+    setIsSubmitted(true)
+  }
   return (
     <>
       <section className='contact-container'>
         <h1>Contact Us</h1>
         <h2>Feel free to contact us with questions, potencial partnerships or media inquiries</h2>
-        <form>
+        <div className='form'>
           <div className="inputs">
             <input type="text" name="name" id="name" placeholder='Name' />
             <input type="text" name="email" id="email" placeholder='E-mail' />
           </div>
           <textarea name="" id="" placeholder='Your text'></textarea>
-          <button className='btn'>SUBMIT</button>
-        </form>
+          <button className='btn' onClick={handleSubmit}>SUBMIT</button>
+        </div>
       </section>
-      {/* <img src={wheel} alt="" className='wheel'/> */}
+      <img src={wheel} alt="" className={`wheel ${isSubmitted ? 'wheel-animation' : ''}`}/>
 
     </>
   )
