4516d7b00e1bc5df3038e8f378acce855fefc7fc
KiwiScreams <screamskiwi3@gmail.com>
2024-09-03 23:28:06 +0400

    loader

---
+++ src/components/loader/Loader.jsx
@@ -0,0 +1,14 @@
+import "./Loader.css";
+const Loader = () => {
+  return (
+    <>
+      <div>
+        <section className="loader">
+          <h1>Vegan World</h1>
+        </section>
+      </div>
+    </>
+  );
+};
+
+export default Loader;
