7acfdd6c66cbb2749d159bab2a7ea7f4a2a3a9bf
KiwiScreams <screamskiwi3@gmail.com>
2024-09-03 23:29:02 +0400

    loader

--- src/components/loader/Loader.jsx
+++ src/components/loader/Loader.jsx
@@ -1,11 +1,30 @@
+import { useEffect, useState } from "react";
 import "./Loader.css";
 const Loader = () => {
+  const [show, setShow] = useState(true);
+
+  useEffect(() => {
+    setTimeout(() => {
+      setShow(false);
+    }, 3000);
+  }, []);
+
+  useEffect(() => {
+    if (show) {
+      document.body.classList.add("no-scroll");
+    } else {
+      document.body.classList.remove("no-scroll");
+    }
+  }, [show]);
+
   return (
     <>
       <div>
-        <section className="loader">
-          <h1>Vegan World</h1>
-        </section>
+        {show ? (
+          <section className="loader">
+            <h1>Vegan World</h1>
+          </section>
+        ) : null}
       </div>
     </>
   );
