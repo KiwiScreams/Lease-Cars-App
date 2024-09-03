c4f2bd9bfc2163e4162ceb0344c5d8d64e80c966
KiwiScreams <screamskiwi3@gmail.com>
2024-09-03 23:50:43 +0400

    loader in newcars page

--- src/pages/new-cars-page/NewCars.jsx
+++ src/pages/new-cars-page/NewCars.jsx
@@ -2,10 +2,12 @@ import "./NewCars.css";
 import { cars } from "../../assets/cars";
 import Cart from "../../components/car/CarList";
 import { useState, useEffect } from "react";
+import Loader from "../../components/loader/Loader";
 
 function NewCars() {
   const [selectedCategory, setSelectedCategory] = useState("");
   const [rows, setRows] = useState(1);
+  const [loading, setLoading] = useState(true);
 
   const handleCategoryChange = (category) => {
     setSelectedCategory(category);
@@ -22,76 +24,85 @@ function NewCars() {
       return car.category === selectedCategory;
     }
   });
-
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
   return (
     <>
-      <section className="section-car">
-        <h1>Choose your car</h1>
-        <div className="category-buttons">
-          <button
-            className={selectedCategory === "" ? "active" : ""}
-            onClick={() => handleCategoryChange("")}
-          >
-            All
-          </button>
-          <button
-            className={selectedCategory === "minivan" ? "active" : ""}
-            onClick={() => handleCategoryChange("minivan")}
-          >
-            Car and Minivan
-          </button>
-          <button
-            className={selectedCategory === "trucks" ? "active" : ""}
-            onClick={() => handleCategoryChange("trucks")}
-          >
-            Trucks
-          </button>
-          <button
-            className={selectedCategory === "suv" ? "active" : ""}
-            onClick={() => handleCategoryChange("suv")}
-          >
-            Crossovers & SUVs
-          </button>
-          <button
-            className={selectedCategory === "electrified" ? "active" : ""}
-            onClick={() => handleCategoryChange("electrified")}
-          >
-            Electrified
-          </button>
-          <div style={{ display: "flex", gap: "4px" }}>
+      {loading ? (
+        <Loader />
+      ) : (
+        <section className="section-car">
+          <h1>Choose your car</h1>
+          <div className="category-buttons">
             <button
-              className={rows === 1 ? "active" : ""}
-              onClick={() => handleRowsChange(1)}
+              className={selectedCategory === "" ? "active" : ""}
+              onClick={() => handleCategoryChange("")}
             >
-              1
+              All
             </button>
             <button
-              className={rows === 2 ? "active" : ""}
-              onClick={() => handleRowsChange(2)}
+              className={selectedCategory === "minivan" ? "active" : ""}
+              onClick={() => handleCategoryChange("minivan")}
             >
-              2
+              Car and Minivan
             </button>
             <button
-              className={rows === 3 ? "active" : ""}
-              onClick={() => handleRowsChange(3)}
+              className={selectedCategory === "trucks" ? "active" : ""}
+              onClick={() => handleCategoryChange("trucks")}
             >
-              3
+              Trucks
             </button>
+            <button
+              className={selectedCategory === "suv" ? "active" : ""}
+              onClick={() => handleCategoryChange("suv")}
+            >
+              Crossovers & SUVs
+            </button>
+            <button
+              className={selectedCategory === "electrified" ? "active" : ""}
+              onClick={() => handleCategoryChange("electrified")}
+            >
+              Electrified
+            </button>
+            <div style={{ display: "flex", gap: "4px" }}>
+              <button
+                className={rows === 1 ? "active" : ""}
+                onClick={() => handleRowsChange(1)}
+              >
+                1
+              </button>
+              <button
+                className={rows === 2 ? "active" : ""}
+                onClick={() => handleRowsChange(2)}
+              >
+                2
+              </button>
+              <button
+                className={rows === 3 ? "active" : ""}
+                onClick={() => handleRowsChange(3)}
+              >
+                3
+              </button>
+            </div>
           </div>
-        </div>
-        <div
-          className="cars-container"
-          style={{ gridTemplateColumns: `repeat(${rows}, 1fr)` }}
-        >
-          {filteredCars.length > 0 ? (
-            filteredCars.map((car) => (
-              <Cart key={car.id} data={car} className={`car-size-${rows}`} />
-            ))
-          ) : (
-            <p className="no-cars">No cars found in this category.</p>
-          )}
-        </div>
-      </section>
+          <div
+            className="cars-container"
+            style={{ gridTemplateColumns: `repeat(${rows}, 1fr)` }}
+          >
+            {filteredCars.length > 0 ? (
+              filteredCars.map((car) => (
+                <Cart key={car.id} data={car} className={`car-size-${rows}`} />
+              ))
+            ) : (
+              <p className="no-cars">No cars found in this category.</p>
+            )}
+          </div>
+        </section>
+      )}
     </>
   );
 }
