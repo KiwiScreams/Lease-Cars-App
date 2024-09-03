82d1d7d32db1daf991db99d367bad47b1838f443
KiwiScreams <screamskiwi3@gmail.com>
2024-09-03 22:14:35 +0400

    about page backgground

--- src/pages/about-page/About.jsx
+++ src/pages/about-page/About.jsx
@@ -1,24 +1,40 @@
-import './About.css'
-import Header from '../../components/header/Header'
+import "./About.css";
+import { useEffect } from "react";
 function AboutUs() {
+  useEffect(() => {
+    document.body.classList.add("about-page");
+    return () => {
+      document.body.classList.remove("about-page");
+    };
+  }, []);
   return (
     <>
-      <Header />
       <section className="about _flex">
-        <div className='about-section'>
+        <div className="about-section">
           <div className="inner-container">
             <h1>About Us</h1>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi, dolores. Sapiente accusantium voluptas, doloribus sequi similique eius porro, nihil, dolore voluptates veritatis minima sit voluptatibus. Eius laboriosam et maxime a.</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi,
+              dolores. Sapiente accusantium voluptas, doloribus sequi similique
+              eius porro, nihil, dolore voluptates veritatis minima sit
+              voluptatibus. Eius laboriosam et maxime a.
+            </p>
             <div className="skills">
-              <span><i className="fa-solid fa-temperature-quarter"></i></span>
-              <span><i className="fa-solid fa-gauge"></i></span>
-              <span><i className="fa-solid fa-bolt"></i></span>
+              <span>
+                <i className="fa-solid fa-temperature-quarter"></i>
+              </span>
+              <span>
+                <i className="fa-solid fa-gauge"></i>
+              </span>
+              <span>
+                <i className="fa-solid fa-bolt"></i>
+              </span>
             </div>
           </div>
         </div>
       </section>
     </>
-  )
+  );
 }
 
-export default AboutUs
+export default AboutUs;
