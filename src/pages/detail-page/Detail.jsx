a8b961f4c56f47f1c5f7c8de5b3d1743f4328600
KiwiScreams <screamskiwi3@gmail.com>
2024-09-03 23:38:09 +0400

    loader in detail page

--- src/pages/detail-page/Detail.jsx
+++ src/pages/detail-page/Detail.jsx
@@ -1,36 +1,46 @@
-import "./Detail.css"
-import React, { useEffect, useState } from "react"
-import { useParams } from "react-router-dom"
-import { cars } from "../../assets/cars"
+import "./Detail.css";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { cars } from "../../assets/cars";
+import Loader from "../../components/loader/Loader";
 function Detail() {
-    const { name } = useParams();
-    console.log(name);
-    const [detail, setDetail] = useState([]);
-    useEffect(() => {
-        const findDetail = cars.filter(car => car.car === name);
-        if (findDetail.length > 0) {
-            setDetail(findDetail[0]);
-        }
-        else {
-            window.location.href = '/';
-        }
-    }, [name])
-    return (
-        <>
-            <div className="car-detail-container">
-                <div className="car-detail-left">
-                    <div className="car-detail-image">
-                        <img src={detail.image} alt="" />
-                    </div>
-                </div>
-                <div className="car-detail-info">
-                    <h1>{detail.name}</h1>
-                    <p>starting price {detail.price} $</p>
-                    <p>{detail.description}</p>
-                </div>
-
+  const { name } = useParams();
+  console.log(name);
+  const [detail, setDetail] = useState([]);
+  useEffect(() => {
+    const findDetail = cars.filter((car) => car.car === name);
+    if (findDetail.length > 0) {
+      setDetail(findDetail[0]);
+    } else {
+      window.location.href = "/";
+    }
+  }, [name]);
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 2000);
+    return () => clearTimeout(timer);
+  }, []);
+  return (
+    <>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div className="car-detail-container">
+          <div className="car-detail-left">
+            <div className="car-detail-image">
+              <img src={detail.image} alt="" />
             </div>
-        </>
-    )
+          </div>
+          <div className="car-detail-info">
+            <h1>{detail.name}</h1>
+            <p>starting price {detail.price} $</p>
+            <p>{detail.description}</p>
+          </div>
+        </div>
+      )}
+    </>
+  );
 }
-export default Detail
+export default Detail;
